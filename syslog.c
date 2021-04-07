180b57d8c25e2ff493db8259d2a2e5b77a84a324
Lucas Gaudet <69484672+L-Gaudet@users.noreply.github.com>
2021-04-06 18:52:29 -0700

    Lucas' syslog.c

--- syslog.c
+++ syslog.c
@@ -2,38 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	
-	if( argc == 1){
-		printf("Error. Please enter a file name ");
-		exit(-1);
-	}
-	
-	else if (argc == 2){
-		
-		//argv[1] is the filename 
 
-		FILE *fp;
-		
-		fp = fopen(argv[1], "w+");
-		
-		if( fp == NULL) {
-			syslog(LOG_ERR, "error - No such file or directory");
-		}
-		else{
-		
-			syslog(LOG_INFO, "info message %s opened", argv[1]);
-			
-			fclose(fp);
-			
-			syslog(LOG_INFO , "info message %s closed", argv[1]);
-		}
-	}
-	
-	else {
-		printf("Error: too many arguments");
-		exit(-1);
-	}
-	
-	exit(0);
+int main(int argc, char *argv[]) {
+  // checks if user entered proper arguments
+  if (argc == 1){
+    printf("Error: no filename specified\n");
+    exit(-1);
+  }
+  else if (argc > 2) {
+    printf("Error: too many arguments\n");
+    exit(-2);
+  }
+
+  //char filename = argv[1];
+
+  FILE *fp;
+  fp = fopen(argv[1], "r");
+
+  if (fp == NULL) {
+    syslog(LOG_ERR, "Error: file does not exist\n");
+    fclose(fp);
+    exit(1);
+  }
+
+  syslog(LOG_INFO, "info message %s opened\n", argv[1]);
+  fclose(fp);
+  syslog(LOG_INFO, "info message %s closed\n", argv[1]);
+
+  exit(0);
 }
