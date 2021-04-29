50b2d8e1b3901f6b4291e81906e778cefbd18816
Lucas Gaudet <lgaudet@chapman.edu>
2021-04-28 20:38:02 -0700

    client v1

--- senseHatClient.cpp
+++ senseHatClient.cpp
@@ -9,6 +9,7 @@
 
 // client side program
 
+
 int main(int argc, char *argv[]); {
 
     int sockfd;
@@ -52,12 +53,67 @@ int main(int argc, char *argv[]); {
         case "1":
           opt = "Get Temperature";
           write(sockfd, &opt, 20);
-          cout << "waiting for server..." << endl;
+          cout << "waiting for server...\n" << endl;
           while(opt=="Get Temperature"){
               read(sockfd, &opt, 20);
           }
-          cout << "Temperature is: " << opt << "degrees celsius" << endl;
+          cout << "Temperature is: " << opt << endl;
+          break;
+
+        case "2":
+          opt = "Get Pressure";
+          write(sockfd, &opt, 20);
+          cout << "waiting for server...\n" << endl;
+          while(opt=="Get Pressure"){
+              read(sockfd, &opt, 20);
+          }
+          cout << "Pressure is: " << opt << endl;
+          break;
+
+        case "3":
+          opt = "Get Humidity";
+          write(sockfd, &opt, 20);
+          cout << "waiting for server...\n" << endl;
+          while(opt=="Get Humidity"){
+              read(sockfd, &opt, 20);
+          }
+          cout << "Humidity is: " << opt << endl;
+          break;
+
+        case "4":
+          opt = "Set Message";
+          write(sockfd, &otp, 20);
+          cout << "waiting for server...\n" << endl;
+          while(opt=="Set Message"){
+              read(sockfd, &opt, 20);
+          }
+          cout << opt;
+          cin >> opt;
+          cout << endl;
+          char[] msg = opt;
+          write(sockfd, &opt, 50);
+          cout << "waiting for server...\n" << endl;
+          while(opt==msg){
+              read(sockfd, &opt, 20);
+          }
+          cout << opt << endl;
+          break;
+
+        case "5":
+          opt = "Exit";
+          write(sockfd, &opt, 20);
+          exit(99);
+
+        default:
+          char[] invalid = opt;
+          write(sockfd, &opt, 20);
+          cout << "waiting for server...\n" << endl;
+          while(opt==invalid){
+              read(sockfd, &opt, 20);
+          }
+          cout << opt << endl;
       }
+      cout << "\n\n"; 
 
     }
 
